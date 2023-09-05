6b3f5e9f4b8cc2e25980988a29922423dcea91f8
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-09-05 11:28:50 +0100

    modified properties file to set up mysql database and annotated patients class

--- src/main/java/com/HealthCare/HealthCare/model/entities/Patient.java
+++ src/main/java/com/HealthCare/HealthCare/model/entities/Patient.java
@@ -1,10 +1,21 @@
 package com.HealthCare.HealthCare.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
 
+@Getter
+@Entity
 public class Patient {
 
-    private int id;
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String surname;
     private LocalDate dateOfBirth;
@@ -12,4 +23,9 @@ public class Patient {
     private String address;
     private String phone;
     private String email;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
