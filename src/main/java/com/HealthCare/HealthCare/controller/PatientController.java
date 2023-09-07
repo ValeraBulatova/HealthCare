31dd164129de7223be88af9cd6c7b1ac8d46686e
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-09-07 14:46:42 +0100

    added the post method and fields to input new patient info

--- src/main/java/com/HealthCare/HealthCare/controller/PatientController.java
+++ src/main/java/com/HealthCare/HealthCare/controller/PatientController.java
@@ -4,6 +4,7 @@ import com.HealthCare.HealthCare.model.entities.Patient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PatientController {
@@ -17,5 +18,13 @@ public class PatientController {
                         "address", "phone", "email"));
 
         return "patients";
+    }
+
+    @PostMapping("/patients/add")
+    public String addPatient(Patient patient) {
+        // Save the new patient to the database
+        //patientRepository.save(patient);
+        System.out.println(patient.getId());
+        return "redirect:/patients"; // Redirect back to the patient list
     }
 }
