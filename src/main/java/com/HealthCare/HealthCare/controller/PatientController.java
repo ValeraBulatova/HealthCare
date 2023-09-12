4eb67e02874d1287faf0c99b3f82e0b99e78d8b3
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-09-12 19:25:40 +0100

    placed patient related pages in patients folder, changed mapping of patient controller methods

--- src/main/java/com/HealthCare/HealthCare/controller/PatientController.java
+++ src/main/java/com/HealthCare/HealthCare/controller/PatientController.java
@@ -1,13 +1,13 @@
 package com.HealthCare.HealthCare.controller;
 
 import com.HealthCare.HealthCare.model.entities.Patient;
-import com.HealthCare.HealthCare.service.PatientRepository;
 import com.HealthCare.HealthCare.service.PatientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/patients")
 public class PatientController {
 
     private PatientService patientService;
@@ -16,22 +16,23 @@ public class PatientController {
         this.patientService = patientService;
     }
 
-    @GetMapping("/patients")
-    public String showPatientView(Model m){
+    // the default page for /patients is the list of patients
+    @GetMapping
+    public String showPatientsView(Model m){
         // Add the list of patients to the model
         m.addAttribute("Patient", patientService.getPatients());
 
-        return "patients";
+        return "/patients/list";
     }
 
-    @GetMapping("/add-patient")
+    @GetMapping("/add")
     public String showAddPatientView(){
         //m.addAttribute("Patient", new Patient());
 
-        return "add-patient";
+        return "/patients/add";
     }
 
-    @PostMapping("/add-patient/add")
+    @PostMapping("/add/confirm")
     public String addPatient(Patient patient) {
         // Save the new patient to the database
         patientService.createPatient(patient);
@@ -39,7 +40,7 @@ public class PatientController {
         return "redirect:/patients"; // Redirect back to the patient list
     }
 
-    @PostMapping("/patients/delete")
+    @PostMapping("/delete")
     public String deletePatient(@RequestParam Long id) {
         patientService.deletePatient(id);
 
